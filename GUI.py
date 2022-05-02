5e4659124a66971c935589bd86edd211ffc7c066
PizzaPastaMafia <lorendelfo@protonmail.com>
2022-05-02 17:21:30 +0200

    started working to a proper gui

--- GUI.py
+++ GUI.py
@@ -2,91 +2,23 @@ import tkinter as tk
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-class MyClass():
-
-    def __init__(self):
-
-        self.sheets = [[1,2,3], [3,1,2], [1,5,1]]
-        self.W = 2
-        self.L = 5
-        self.all_figures = []
-
-    def plot_sheet(self, data):
-        """plot single figure"""
-
-        fig, ax = plt.subplots(1)
-
-        ax.set_xlim([0, self.W]) 
-        ax.set_ylim([0, self.L])
-
-        ax.plot(data)
-
-        return fig
-
-    def generate_all_figures(self):
-        """create all figures and keep them on list"""
-
-        for data in self.sheets:
-            fig = self.plot_sheet(data)
-            self.all_figures.append(fig)
-
-def show_figure(number):
-    global dataPlot
-
-    # remove old canvas
-    if dataPlot is not None: # at start there is no canvas to destroy
-        dataPlot.get_tk_widget().destroy()
-
-    # get figure from list
-    one_figure = my_class.all_figures[number]
-
-    # display canvas with figuere
-    dataPlot = FigureCanvasTkAgg(one_figure, master=window)
-    dataPlot.draw()
-    dataPlot.get_tk_widget().grid(row=0, column=0)
-
-def on_prev():
-    global selected_figure
-
-    # get number of previous figure
-    selected_figure -= 1
-    if selected_figure < 0:
-        selected_figure = len(my_class.all_figures)-1
-
-    show_figure(selected_figure)
-
-def on_next():
-    global selected_figure
-
-    # get number of next figure
-    selected_figure += 1
-    if selected_figure > len(my_class.all_figures)-1:
-        selected_figure = 0
-
-    show_figure(selected_figure)
-
-# --- main ---
-
-my_class = MyClass()
-my_class.generate_all_figures()
-
 window = tk.Tk()
-window.rowconfigure(0, minsize=500)    # minimal height
-window.columnconfigure(0, minsize=700) # minimal width
+window.title("Lorenzo Del Forno Dynamic Stock Trading Simulator")
 
-# display first figure    
-selected_figure = 0
-dataPlot = None # default value for `show_figure`
-show_figure(selected_figure)
+mainFrame = tk.Frame(master= window, width=1360, height=750, bg="white")
+mainFrame.pack(fill="both")
 
-# add buttons to change figures
-frame = tk.Frame(window)
-frame.grid(row=1, column=0)
+leftSide = tk.Frame(master= mainFrame, width=260, height=750, bg="blue")
+leftSide.pack(fill="y", side="left")
 
-b1 = tk.Button(frame, text="<<", command=on_prev)
-b1.grid(row=0, column=0)
+controlPanel = tk.Frame(master= leftSide, width=260, height=100, bg="white")
+controlPanel.pack(fill="y", side="top")
 
-b2 = tk.Button(frame, text=">>", command=on_next)
-b2.grid(row=0, column=1)
+stockGeneral = tk.Frame(master= mainFrame, width=700, bg="yellow")
+stockGeneral.pack(fill="y", side="left")
+
+stockAdvanced = tk.Frame(master= mainFrame, width=300, bg="red")
+stockAdvanced.pack(fill="y", side="left")
+
 
 window.mainloop()
--- LDFDSTS.py
+++ LDFDSTS.py
@@ -2,15 +2,3 @@ import plotly.graph_objects as go
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-tsla = yf.Ticker("TSLA")
-hist = tsla.history(period='5y')
-print(hist.head())
-
-#fig = go.Figure(data = go.Scatter(x=hist.index, y = hist['Close']))
-#fig.show()
-
-x=hist.index
-y = hist['Close']
-
-plt.plot(x, y)
-plt.show()
