2b460bafd5c6096bdca3d7b6818bb7ae47006a94
PizzaPastaMafia <lorendelfo@protonmail.com>
2022-05-04 22:03:35 +0200

    idk what I'm doing

--- GUI.py
+++ GUI.py
@@ -1,44 +1,53 @@
 import sys
 
 # 1. Import `QApplication` and all the required widgets
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QDialog
-from PyQt5.QtWidgets import QDialogButtonBox
-from PyQt5.QtWidgets import QFormLayout
-from PyQt5.QtWidgets import QLineEdit
-from PyQt5.QtWidgets import QVBoxLayout
-from PyQt5.QtWidgets import QHBoxLayout
+#from PyQt5.QtWidgets import QApplication
+#from PyQt5.QtWidgets import QDialog
+#from PyQt5.QtWidgets import QDialogButtonBox
+#from PyQt5.QtWidgets import QFormLayout
+#from PyQt5.QtWidgets import QLineEdit
+#from PyQt5.QtWidgets import QVBoxLayout
+#from PyQt5.QtWidgets import QHBoxLayout
+#from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QFrame, QMainWindow, QApplication, QDialog, QDialogButtonBox, QFormLayout, QLineEdit, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt5.QtGui import  QPalette, QColor
+
+import PyQt5
+
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-class GUI(QDialog):
-    """Dialog."""
-    def __init__(self, parent=None):
-        """Initializer."""
-        super().__init__(parent)
-        self.setWindowTitle('Lorenzo Del Forno Dynamic Stock Trading Simulator')
-        
-        mainFrame = QHBoxLayout()
-        mainFrame.setStyleSheet("background:white")
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
-        leftSide = QVBoxLayout()
-        leftSide.setStyleSheet("background:blue")
+        self.setWindowTitle("My App")
 
-        stockGeneral = QVBoxLayout()
-        stockGeneral.setStyleSheet("background:yellow")
+        layout1 = QHBoxLayout()
+        layout2 = QVBoxLayout()
+        layout3 = QVBoxLayout()
 
-        stockAdvanced = QVBoxLayout()
-        stockGeneral.setStyleSheet("background:red")
+        bg = QWidget(QFrame(None))
+        bg.setStyleSheet("background-color: yellow;")
+        layout2.addWidget(bg)
+        layout2.addWidget()
+        layout2.addWidget()
 
-        mainFrame.setLayout(leftSide)
-        mainFrame.setLayout(stockGeneral)
-        mainFrame.setLayout(stockAdvanced)
+        layout1.addLayout( layout2 )
 
-        
-        self.setLayout(mainFramw)
+        layout1.addWidget()
+
+        layout3.addWidget()
+        layout3.addWidget()
+
+        layout1.addLayout( layout3 )
+
+        widget = QWidget()
+        widget.setLayout(layout1)
+        self.setCentralWidget(widget)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    dlg = GUI()
+    dlg = MainWindow()
     dlg.show()
     sys.exit(app.exec_())
